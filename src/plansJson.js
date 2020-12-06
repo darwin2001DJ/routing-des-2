2efa3ad05ea47ac2ae85e910a3903ec27295c3c5
Your Name <darwinmuthu450@gmail.com>
2020-12-07 05:11:14 +0530

    init

---
+++ src/plansJson.js
@@ -0,0 +1,69 @@
+export const topup = [
+  {
+    id: "topup1",
+    price: "10",
+    plan: "Talktime of Rs.7.47",
+  },
+  {
+    id: "topup2",
+    price: "20",
+    plan: "Talktime of Rs.14.95",
+  },
+  {
+    id: "topup3",
+    price: "100",
+    plan: "Talktime of Rs.81.95",
+  },
+  {
+    id: "topup4",
+    price: "200",
+    plan: "Enjoy talktime of Rs.480",
+  },
+  {
+    id: "topup5",
+    price: "1000",
+    plan: "Enjoy talktime of Rs.960",
+  },
+];
+
+export const gb34 = [
+  {
+    id: "gb3gb41",
+    price: "48",
+    plan: "Enjoy 3GB data, valid till your current pack validity.",
+  },
+  {
+    id: "gb3gb42",
+    price: "98",
+    plan: "Enjoy 12GB data, valid till your current pack validity.",
+  },
+  {
+    id: "gb3gb43",
+    price: "251",
+    plan: "Enjoy 50GB data, valid till your current pack validity.",
+  },
+  {
+    id: "gb3gb44",
+    price: "401",
+    plan:
+      "Enjoy Disney+ Hotstar VIP 1 year subscription. Also get 3GB data for 28 days.",
+  },
+];
+export const gb2 = [
+  {
+    id: "gb21",
+    price: "98",
+    plan: "Enjoy 12GB data, valid till your current pack validity.",
+  },
+  {
+    id: "gb22",
+    price: "251",
+    plan: "Enjoy 50GB data, valid till your current pack validity.",
+  },
+  {
+    id: "gb23",
+    price: "401",
+    plan:
+      "Enjoy Disney+ Hotstar VIP 1 year subscription. Also get 3GB data for 28 days.",
+  },
+];
